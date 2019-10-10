97cf39ede9951b5f7d9a6058d2086ada69562715
Patrick Eli Catach <patrick.catach@gmail.com>
2019-10-10 22:38:18 +0100

    removed unused scripts, renamed files

--- breakpointnumber.py
+++
@@ -1,8 +0,0 @@
-with open('data.txt') as input_data:
-    perm = map(int, input_data.read().strip().lstrip('(').rstrip(')').split())
-
-num = sum(map(lambda x,y: x - y != 1, perm+[len(perm)+1], [0]+perm))
-
-print str(num)
-with open('resp.txt', 'w') as data:
-    data.write(str(num))
--- genomepath.py
+++
@@ -1,6 +0,0 @@
-def spell(path):
-    string = ''
-    for seq in path:
-        string += seq[0]
-    string += seq [1:]
-    print string
